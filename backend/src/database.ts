2881a457cc09597076100a4aa3a5885ab1907c82
Jaime Escoto <jaime.escoto@hotmail.com>
2026-06-12 22:31:03 -0600

    Update database.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,7 +3,6 @@ import { Pool } from 'pg';
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-  family: 4,
 });
 
 export async function initDB() {
